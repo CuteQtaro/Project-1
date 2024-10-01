e936ea86445cc61e9a7225b7dec04583847ffdae
CuteQtaro <Linh.NTD225030@sis.hust.edu.vn>
2024-10-01 08:48:26 +0700

    adding algorithm

--- week 2/Heap's Algorithm.cpp
+++ week 2/Heap's Algorithm.cpp
@@ -1,1 +1,25 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+void generate(vector<int> &a, int n){
+    if(n==1){
+        for(auto x: a) cout<<x<<" ";
+        cout<<endl;
+        return;
+    }
+    for(int i=0; i<n-1; i++){
+        generate(a, n-1);
+        if(n%2==0) swap(a[i], a[n-1]);
+        else swap(a[0], a[n-1]);
+    }
+    generate(a, n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) a[i]=i+1;
+    generate(a, n);
+    return 0;
+}
